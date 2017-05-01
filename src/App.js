7f5a7785adde1fe92d14aa1ed5cb4f7fdb5a50d7
Rene Sanchez <renesanchez@yahoo.com>
2017-05-01 15:30:51 -0500

    third try

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import SearchBar from './components/SearchBar.js';
+import MapDetail from './components/MapDetail.js';
+import RestaurantList from './components/RestaurantList.js';
+import _ from 'lodash';
 import './App.css';
 
+// AIzaSyDoQMExpBJGbyzOSCBLu2RncN5_BN3LhGw
+// https://www.youtube.com/embed/
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+  }
+
+  componentDidMount(){
+    this.search('monkeys');
+  }
+
+  changeSelectedVideo(video){
+    this.setState({
+      selectedVideo: video
+    })
+  }
+
+  search(term){
+    axios.get('https://maps.googleapis.com/maps/api/js',{
+      params: {
+        q: term,
+        key: 'AIzaSyD4Cx5fT60X--BRDV8DjH73VX6SMqV6iL4',
+        libraries: 'places',
+        callback: 'initMap',
+        type: 'video',
+        part: 'snippet'
+      }
+    }).then((response)=>{
+      console.log(response);
+      this.setState({
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+      });
+    }).catch((error)=>{
+      console.error(error);
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce( term=>{ this.search(term) }, 300);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="row">
+          <SearchBar search={videoSearch}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="row">
+          <MapDetail selectedVideo={this.state.selectedVideo}/>
+          <RestaurantList videos={this.state.videos} changeSelectedVideo={this.changeSelectedVideo.bind(this)}/>
+        </div>
       </div>
     );
   }
