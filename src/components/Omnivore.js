0248ac90185b1e3ef74deee7b12b6855ce9c619a
Dan <you@example.com>
2017-05-02 09:36:37 -0500

    progress in database functionality

--- src/components/Omnivore.js
+++ src/components/Omnivore.js
@@ -1,7 +1,47 @@
 import React, { Component } from 'react';
+import firebase, { database, firebaseListToArray } from '../firebase';
 
 class Omnivore extends Component {
+	constructor(props){
+		super(props);
+
+		this.ref = firebase.database().ref();
+		this.state = {
+			food : []
+		}
+	}
+
+	  componentWillMount() {
+    database.ref('/food')
+      .on('value', data => {
+          const results = firebaseListToArray(data.val());
+          console.log('food', results);
+
+          this.setState({
+            food: results
+          });
+        });
+  }
+
+	getRandomFood(food){
+		this.ref.on('value', function(snapshot){
+		var i = 0;
+		var rand = Math.floor(Math.random() * snapshot.numChildren());
+		snapshot.forEach(function(snapshot) {
+  		if (i === rand) {
+   		 // picked random item, 
+   		 console.log(food);
+   		const snapFood =  snapshot.val()
+  		}
+  		i++;
+			});
+		});
+	}
+
 	render(){
+		const food = this.state.food.map((food) => {
+			console.log('food ', food);
+      		return (<a href="#" key={ food.id } name={ food.id }>{this.snapFood}</a>)})
 		return(
 			<div className="row">
 				<h1>So you eat it all huh? What looks good here?</h1>
@@ -9,7 +49,7 @@ class Omnivore extends Component {
 				<h3>I'm hungry for...</h3>
 
 				<div className="col-md-3">
-					<a href="">Pizza</a>
+					{ food }
 				</div>
 				<div className="col-md-3">
 					<a href="">Tacos</a>
--- src/components/Vegetarian.js
+++ src/components/Vegetarian.js
@@ -4,7 +4,8 @@ class Vegetarian extends Component {
 	render(){
 		return(
 			<div className="row">
-				<h1>Ah someone who enjoys a good vegetable or two. What looks good to you here?</h1>
+				<h1>Ah someone who enjoys a good vegetable or two.
+				<br/> What looks good to you here?</h1>
 				<h2>Or, let us choose for you.</h2>
 				<h3>I'm thinking...</h3>
 
