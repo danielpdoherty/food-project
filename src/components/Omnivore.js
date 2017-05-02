cc7fc2b56e0c77d50e3ce923956bc6dfe63e4adb
Dan <you@example.com>
2017-05-02 12:11:02 -0500

    firebase connectivity and new public img sources

--- src/components/Omnivore.js
+++ src/components/Omnivore.js
@@ -23,54 +23,62 @@ class Omnivore extends Component {
         });
   }
 
-	getRandomFood(food){
-		this.ref.on('value', function(snapshot){
-		var i = 0;
-		var rand = Math.floor(Math.random() * snapshot.numChildren());
-		snapshot.forEach(function(snapshot) {
-  		if (i === rand) {
-   		 // picked random item, 
-   		 console.log(food);
-   		const snapFood =  snapshot.val()
-  		}
-  		i++;
-			});
-		});
-	}
+	// getRandomFood(food){
+	// 	this.ref.on('value', function(snapshot){
+	// 	var i = 0;
+	// 	var rand = Math.floor(Math.random() * snapshot.numChildren());
+	// 	snapshot.forEach(function(snapshot) {
+ //  		if (i === rand) {
+ //   		 // picked random item, 
+ //   		 console.log(food);
+ //   		let snapFood =  snapshot.val()
+ //   		console.log('snapfood ', snapFood);
+ //  		}
+ //  		i++;
+	// 		});
+	// 	}); 
+	// }
 
 	render(){
 		const food = this.state.food.map((food) => {
-			console.log('food ', food);
-      		return (<a href="#" key={ food.id } name={ food.id }>{this.snapFood}</a>)})
+			console.log('food map', food);
+      		let foodLength = food.id;
+      		if(foodLength.length < 9){
+      		return (
+      			<div className="col-md-3">	
+      				<a href="#" key={ food.id } name={ food.id }>
+							<img src={food.properties.imgUrl} alt="Food" />
+							{ food.properties.name }
+      				</a>
+      			</div>
+      		)}});
 		return(
 			<div className="row">
 				<h1 className="animate">So you eat it all huh? What looks good here?</h1>
 				<h2>Or, let us choose for you.</h2>
 				<h3>I&#8217;m hungry for...</h3>
 
+				
 				<div className="col-md-3">
-					{ food }
+					<a href="">{ food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Tacos</a>
+					<a href="">{ food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Steak</a>
+					<a href="">{food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Burgers</a>
+					<a href="">{ food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Kung Pao Chicken</a>
+					<a href="">{ food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Pulled Pork</a>
+					<a href="">{ food }</a>
 				</div>
 				<div className="col-md-3">
-					<a href="">Kebab</a>
-				</div>
-				<div className="col-md-3">
-					<a href="">Chicken Wings</a>
+					<a href="">{ food }</a>
 				</div>
 			</div>
 
--- src/components/Vegetarian.js
+++ src/components/Vegetarian.js
@@ -12,13 +12,10 @@ class Vegetarian extends Component {
 	render(){
 		return(
 			<div className="row">
-<<<<<<< HEAD
-				<h1>Ah someone who enjoys a good vegetable or two.
-				<br/> What looks good to you here?</h1>
-=======
+
 				<h1 className="animate">Ah someone who enjoys a good vegetable or two.</h1>
 				<h1 className="animate"> What looks good to you here?</h1>
->>>>>>> dev
+
 				<h2>Or, let us choose for you.</h2>
 				<h3>I&#8217;m thinking...</h3>
 
