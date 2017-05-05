993e3010e7f787e984af3864631bed4674e5afdf
komal jadvani <komaljadvani3@gmail.com>
2017-05-05 12:08:16 -0500

    change css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,39 +2,47 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import omni from '../img/omni.png';
 import vege from '../img/vege.jpg';
+import vegge from '../img/Veggies.jpg';
+import meat from '../img/meat.jpg';
+import backgroundimg from '../img/backgroundimg.jpg';
 import './typingAnim.css';
+import Slider from 'react-slick';
+
 
 class Home extends Component {
 
 	render(){
+		const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+			autoplay: true,
+			arrows: false
+
+    };
+
 		return(
 			<div>
 				<div className="row">
 					<h1 className="animate">Feeling Hungry and can&#8217;t decide?</h1>
-					<h2>I <em>hate</em> that!
-					<br />
-						Let&#8217;s help you find some grub!
-					</h2>
-					<h3>Let&#8217;s start with some basic info...
-					<br />
-						You are a ...
-					</h3>
+					<Slider {...settings}>
+					<img src={vegge} alt="Omnivore"/>
+					<img  src={meat} alt="Omnivore" />
+					</Slider>
 				</div>
 				<div className="row">
-					<div className="col-md-6 choiceBox" id="choices">
+					<div className="col-md-6">
 						<Link to="/omni">
-						<img src={omni} alt="Omnivore"/>
-						<br/>
-						Omnivore!
-						</Link>
-					</div>
-					<div className="col-md-6 choiceBox" id="choices">
-						<Link to="/vege">
-						<img src={vege} alt="Vegetarian"/>
-						<br/>
-						Vegetarian!
-						</Link>
-					</div>
+						<button type="button" class="btn btn-primary">Omnivore!</button>
+            </Link>
+						</div>
+						<div className="col-md-6">
+							<Link to="/vege">
+							<button type="button" class="btn btn-primary">Vegetarian</button>
+	            </Link>
+							</div>
 				</div>
 			</div>
 			);
